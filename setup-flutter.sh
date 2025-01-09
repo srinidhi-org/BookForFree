baa1e727412671af0731699295955fd06eff8b74
grgowthamkrishna <grgowthamkrishna@gmail.com>
2025-01-09 11:51:29 +0530

    bash and toml modification

--- setup-flutter.sh
+++ setup-flutter.sh
@@ -2,8 +2,19 @@
 
 # Download Flutter SDK
 FLUTTER_URL="https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_3.10.6-stable.tar.xz"
-wget -q -O flutter.tar.xz $FLUTTER_URL
-tar xf flutter.tar.xz
+wget -q -O flutter_sdk.tar.xz $FLUTTER_URL
+
+# Extract Flutter SDK
+tar xf flutter_sdk.tar.xz
+
+# Add Flutter to PATH
 export PATH="$PATH:$(pwd)/flutter/bin"
 
-# Optionally, you might want to run `flutter doctor` to verify setup, but it's not necessary for CI
+# Verify Flutter installation
+flutter --version
+
+# Clean up the downloaded archive
+rm flutter_sdk.tar.xz
+
+# Build the Flutter web app
+flutter build web --release
